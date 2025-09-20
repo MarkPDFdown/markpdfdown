fbb1a6d4b8434468c950cac6d3180c80eae7e741
Jorben <jorbenzhu@gmail.com>
2025-09-20 14:58:51 +0800

    chore: update version

--- src/markpdfdown/__init__.py
+++ src/markpdfdown/__init__.py
@@ -3,7 +3,7 @@ MarkPDFDown - A powerful tool that leverages multimodal large language models
 to transcribe PDF files into Markdown format.
 """
 
-__version__ = "1.1.1"
+__version__ = "1.1.2"
 __author__ = "MarkPDFDown Team"
 __email__ = "jorbenzhu@gmail.com"
 __description__ = "Convert PDF and images to Markdown using multimodal LLMs"
